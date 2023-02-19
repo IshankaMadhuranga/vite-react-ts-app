234d24ddb6696c7458010e374577dcd1d6dd6915
Ishanka Madhuranga <ishanka@ascentic.se>
2023-02-19 22:18:13 +0530

    initial commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+const About = (): JSX.Element => {
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">About page</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+const Home = (): JSX.Element => {
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">Home page</p>
+    </div>
+  )
+}
+
+export default Home
